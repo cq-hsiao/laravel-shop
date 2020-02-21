0b7766a9f21cc87ff72367117fb73c35f81f6430
“cq-hsiao” <124328227@qq.com>
2020-02-21 06:55:30 +0000

    注册和登录

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','PagesController@root')->name('root');
 
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
