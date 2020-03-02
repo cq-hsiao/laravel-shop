75d02bd84420c5b61befcf98b42e7980e84fc8d1
“cq-hsiao” <124328227@qq.com>
2020-03-02 09:54:33 +0000

    用户界面 - 收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -99,4 +99,10 @@ class ProductsController extends Controller
         return [];
     }
 
+    public function favorites(Request $request)
+    {
+        $products = $request->user()->favoriteProducts()->paginate(16);
+
+        return view('products.favorites', ['products' => $products]);
+    }
 }
