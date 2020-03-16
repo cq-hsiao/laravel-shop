ff45b031c54bc529e02996749b44fb8c6ffcb98a
“cq-hsiao” <124328227@qq.com>
2020-03-16 02:37:03 +0000

    关闭未支付订单、订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\ProductSku;
@@ -14,6 +15,24 @@ use Illuminate\Support\Facades\DB;
 
 class OrdersController extends Controller
 {
+
+    public function index(Request $request)
+    {
+//        $orders = Order::query()
+//            // 使用 with 方法预加载，避免N + 1问题
+//            ->with(['items.product', 'items.productSku'])
+//            ->where('user_id', $request->user()->id)
+//            ->orderBy('created_at', 'desc')
+//            ->paginate();
+
+        $orders = $request->user()->orders()
+            ->with(['items.product','items.productSku'])
+            ->orderBy('created_at','desc')
+            ->paginate();
+        
+        return view('orders.index',['orders' => $orders]);
+    }
+
     public function store(OrderRequest $request)
     {
         $user = $request->user();
@@ -75,6 +94,9 @@ class OrdersController extends Controller
 
             return $order;
         });
+
+        // 创建订单之后触发任务
+        $this->dispatch(new CloseOrder($order,config('app.order_ttl')));
 
         return $order;
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,10 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(CartItem::class);
     }
+
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
