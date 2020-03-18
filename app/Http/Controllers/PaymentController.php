3474eee1c844194d23d4e9259072b48a3ab52a98
“cq-hsiao” <124328227@qq.com>
2020-03-18 07:18:54 +0000

    微信支付

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,21 +5,38 @@ namespace App\Http\Controllers;
 use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Carbon\Carbon;
+use Endroid\QrCode\QrCode;
 use Illuminate\Http\Request;
 
 
 class PaymentController extends Controller
 {
-    public function payByAlipay(Order $order,Request $request)
-    {
+
+    protected function checkOrderStatus($order){
+
         // 判断订单是否属于当前用户
         $this->authorize('own',$order);
 
         // 订单已支付或者已关闭
-        if($order->pay_at || $order->closed)
+        if($order->paid_at || $order->closed)
         {
-            throw new InvalidRequestException('订单已支付或已关闭');
+            $msg = $order->paid_at ? '该订单已支付' : '该订单已关闭';
+            throw new InvalidRequestException($msg);
         }
+
+    }
+
+    public function payByAlipay(Order $order,Request $request)
+    {
+//        // 判断订单是否属于当前用户
+//        $this->authorize('own',$order);
+//
+//        // 订单已支付或者已关闭
+//        if($order->paid_at || $order->closed)
+//        {
+//            throw new InvalidRequestException('订单已支付或已关闭');
+//        }
+        $this->checkOrderStatus($order);
 
         return app('alipay')->web([
             'out_trade_no' => $order->no, // 订单编号，需保证在商户端不重复
@@ -27,6 +44,24 @@ class PaymentController extends Controller
             'subject'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单标题
         ]);
 
+    }
+
+    public function payByWechat(Order $order, Request $request) {
+
+        $this->checkOrderStatus($order);
+
+        // scan 方法为拉起微信扫码支付
+        $wechatOrder = app('wechat_pay')->scan([
+            'out_trade_no' => $order->no,  // 商户订单流水号，与支付宝 out_trade_no 一样
+            'total_fee' => $order->total_amount * 100, // 与支付宝不同，微信支付的金额单位是分。
+            'body'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单描述
+        ]);
+
+        // 把要转换的字符串作为 QrCode 的构造函数参数
+        $qrCode = new QrCode($wechatOrder->code_url);
+
+        // 将生成的二维码图片数据以字符串形式输出，并带上相应的响应类型
+        return response($qrCode->writeString(), 200, ['Content-Type' => $qrCode->getContentType()]);
     }
 
     // 前端回调页面
@@ -75,5 +110,31 @@ class PaymentController extends Controller
         ]);
 
         return app('alipay')->success();
+    }
+
+    public function wechatNotify()
+    {
+        // 校验回调参数是否正确
+        $data  = app('wechat_pay')->verify();
+        // 找到对应的订单
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 订单不存在则告知微信支付
+        if (!$order) {
+            return 'fail';
+        }
+        // 订单已支付
+        if ($order->paid_at) {
+            // 告知微信支付此订单已处理
+            return app('wechat_pay')->success();
+        }
+
+        // 将订单标记为已支付
+        $order->update([
+            'paid_at'        => Carbon::now(),
+            'payment_method' => 'wechat',
+            'payment_no'     => $data->transaction_id,
+        ]);
+
+        return app('wechat_pay')->success();
     }
 }
