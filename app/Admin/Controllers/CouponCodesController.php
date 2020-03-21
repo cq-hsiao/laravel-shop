e83d5c5046e83e2ebb743fd192baab4404d7f6f6
“cq-hsiao” <124328227@qq.com>
2020-03-21 03:48:18 +0000

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -3,11 +3,12 @@
 namespace App\Admin\Controllers;
 
 use App\Models\CouponCode;
+use Encore\Admin\Facades\Admin;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
-use Encore\Admin\Show;
+
 
 class CouponCodesController extends AdminController
 {
@@ -53,10 +54,94 @@ class CouponCodesController extends AdminController
         $grid->created_at('创建时间');
 
         $grid->actions(function ($actions) {
-            $actions->disableView();
+//            $actions->disableView();
         });
 
         return $grid;
+    }
+
+    protected function form()
+    {
+        $form = new Form(new CouponCode());
+
+        $form->tools(function (Form\Tools $tools) {
+
+            // 去掉`查看`按钮
+//            $tools->disableView();
+        });
+
+        $form->display('id','ID');
+        $form->text('name', '名称')->rules('required');
+//        $form->text('code', '优惠码')->rules('nullable|unique:coupon_codes');
+        $form->text('code', '优惠码')->rules(function ($form){
+            // 如果 $form->model()->id 不为空，代表是编辑操作
+            if($id = $form->model()->id){
+                return 'nullable|unique:coupon_codes,code,'.$id.',id';
+            } else {
+                return 'nullable|unique:coupon_codes';
+            }
+        });
+
+        $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required')->default(CouponCode::TYPE_FIXED);
+        $form->text('value', '折扣')->rules(function ($form) {
+            if (request()->input('type') === CouponCode::TYPE_PERCENT) {
+                // 如果选择了百分比折扣类型，那么折扣范围只能是 1 ~ 99
+                return 'required|numeric|between:1,99';
+            } else {
+                // 否则只要大等于 0.01 即可
+                return 'required|numeric|min:0.01';
+            }
+        });
+        $form->text('total', '总量')->rules('required|numeric|min:0');
+        $form->text('min_amount', '最低金额')->rules('required|numeric|min:0');
+        $form->datetime('not_before', '开始时间');
+        $form->datetime('not_after', '结束时间')->rules(function ($form){
+            if(request()->input('not_before') && request()->input('not_after')){
+                return 'date|after:'.request()->input('not_before');
+            } else {
+                return false;
+            }
+        });
+        $form->radio('enabled', '启用')->options(['1' => '是', '0' => '否']);
+
+        $form->saving(function (Form $form) {
+            if (!$form->code) {
+                $form->code = CouponCode::findAvailableCode();
+            }
+        });
+
+//        $form->disableViewCheck();
+
+        return $form;
+    }
+
+
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('查看优惠券')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(Admin::show(CouponCode::query()->findOrFail($id),function($show){
+                $show->id('ID');
+                $show->name('优惠券名称');
+                $show->code('优惠码');
+                $show->type('类型')->as(function($value) {
+                    return CouponCode::$typeMap[$value];
+                });
+
+                // 根据不同的折扣类型用对应的方式来展示
+                $show->value('折扣')->as(function($value){
+                    return $this->type === CouponCode::TYPE_FIXED ? '￥'.$value : $value.'%';
+                });
+                $show->description('描述');
+
+                $show->enabled('是否启用')->as(function($value) {
+                    return $value ? '是' : '否';
+                });
+                $show->usage('用量')->as(function ($value) {
+                    return "{$this->used} / {$this->total}";
+                });
+            }));
     }
 
 
