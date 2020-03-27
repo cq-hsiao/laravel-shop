ca81031aa7cc86bdf814399f19111f6bc9ec79fa
“cq-hsiao” <124328227@qq.com>
2020-03-27 08:36:35 +0000

    订单模块调整,增加众筹进度更新监听事件

---
+++ app/Listeners/UpdateCrowdfundingProductProgress.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Models\Order;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class UpdateCrowdfundingProductProgress implements ShouldQueue
+{
+
+    public function __construct()
+    {
+        DB::listen(function($query){
+            Log::info(get_class(),[$query->sql]);
+        });
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPaid  $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        $order = $event->getOrder();
+        // 如果订单类型不是众筹商品订单，无需处理
+        if($order->type != Order::TYPE_CROWDFUNDING){
+            return;
+        }
+
+        $crowdfunding = $order->items()->first()->product->crowdfunding;
+
+        $data = Order::query()
+            // 1、查出订单类型为众筹订单 2、并且是已支付的 3、并且包含了本商品
+            ->where('type',Order::TYPE_CROWDFUNDING)
+            ->whereNotNull('paid_at')
+            ->whereHas('items',function($query) use ($crowdfunding) {
+                $query->where('product_id',$crowdfunding->id);
+            })
+            ->first([
+                // 取出订单总金额,去重的支持用户数
+               DB::raw('sum(total_amount) as total_amount'),
+               DB::raw('count(distinct(user_id)) as user_count'),
+            ]);
+
+        $crowdfunding->update([
+            'total_amount' => $data->total_amount,
+            'user_count'   => $data->user_count,
+        ]);
+    }
+}
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -37,6 +37,7 @@ class OrderService
                 ],
                 'remark'       => $remark,
                 'total_amount' => 0,
+                'type' => Order::TYPE_NORMAL,
             ]);
             // 订单关联到当前用户
             $order->user()->associate($user);
@@ -101,14 +102,15 @@ class OrderService
 
             // 创建一个订单
             $order = new Order([
-               'address' => [
+                'address' => [
                    'address' => $address->full_address,
                    'zip' => $address->zip,
                    'contact_name'  => $address->contact_name,
                    'contact_phone' => $address->contact_phone,
-               ],
-               'remark'       => '',
-               'total_amount' => $productSku->price * $amount,
+                ],
+                'remark'       => '',
+                'total_amount' => $productSku->price * $amount,
+                'type' => Order::TYPE_CROWDFUNDING,
             ]);
 
             // 订单关联到当前用户
