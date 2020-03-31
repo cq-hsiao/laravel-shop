05d9610d45f92e331fb50492c65539a0456df634
“cq-hsiao” <124328227@qq.com>
2020-03-31 06:13:35 +0000

    支付宝分期还款

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -6,8 +6,8 @@
         <div class="card-header">操作成功</div>
         <div class="card-body text-center">
             <h1>{{ $msg }}</h1>
-            <a class="btn btn-primary" href="@if($url) {{ $url }} @else{{ route('root') }}@endif">
-                @if($op) {{ $op }} @else 返回首页 @endif</a>
+            <a class="btn btn-primary" href="@if(isset($url)) {{ $url }} @else{{ route('root') }}@endif">
+                @if(isset($op)) {{ $op }} @else 返回首页 @endif</a>
         </div>
     </div>
 @endsection
