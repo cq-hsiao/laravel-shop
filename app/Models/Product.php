c3b900d6da1d348af523b565251ec4e13b6dbc11
cq-hsiao <124328227@qq.com>
2020-04-07 09:55:40 +0000

    修复分面搜索 Bug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -84,7 +84,8 @@ class Product extends Model
         });
         // 只取出需要的商品属性字段
         $arr['properties'] = $this->properties->map(function ($property) {
-            return Arr::only($property->toArray(), ['name', 'value']);
+            return array_merge(Arr::only($property->toArray(), ['name', 'value']),
+                ['search_value' => $property->name.':'.$property->value]);
         });
 
         return $arr;
